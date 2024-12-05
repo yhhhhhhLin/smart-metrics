62c4c4c8e7695487b1e86b04b90d7045c62a9ef0
linzz <1583427850@qq.com>
2024-12-05 22:50:44 +0800

    init

---
+++ smart-metrics-dubbo-api/src/main/java/xyz/linyh/Main.java
@@ -0,0 +1,7 @@
+package xyz.linyh;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ smart-metrics-service/smart-metrics-user/src/main/java/xyz/linyh/user/UserApplication.java
@@ -0,0 +1,14 @@
+package xyz.linyh.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author linzz
+ */
+@SpringBootApplication
+public class UserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+}
