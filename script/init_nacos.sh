b1a14856d81f992b822ce1b3ac631eed7a50bb75
linzz <1583427850@qq.com>
2024-12-16 21:08:30 +0800

    feat

---
+++ script/init_nacos.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+NACOS_HOST="nacos"
+NACOS_PORT=8848
+NAMESPACE="smart-metrics-default"
+
+# nacos配置文件名
+DEFAULT_CONFIG_PATH="nacos_config.zip"
+
+
+    CONFIG_PATH="$DEFAULT_CONFIG_PATH"
+
+# 在nacos上增加命名空间配置
+curl -X POST "http://$NACOS_HOST:$NACOS_PORT/nacos/v1/console/namespaces" -d "customNamespaceId=$NAMESPACE&namespaceName=$NAMESPACE&namespaceDesc= default config"
+
+echo $CONFIG_PATH
+echo 1234
+# 将配置写到nacos
+curl --location --request POST "http://$NACOS_HOST:$NACOS_PORT/nacos/v1/cs/configs?import=true&namespace=$NAMESPACE" --form "policy=OVERWRITE" --form "file=@$CONFIG_PATH"
